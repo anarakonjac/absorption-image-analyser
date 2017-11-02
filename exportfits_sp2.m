b19809d3643ddfd03d562a57a5764c4685579cca
lab62 <lab62@129.234.190.201>
2017-11-02 12:59:24 +0000

    Fixed export "fit" bug.

--- exportfits_sp2.m
+++ exportfits_sp2.m
@@ -17,10 +17,10 @@ y = ylimits(1):ylimits(2);
 % figure(70)
 % imagesc(Anew_sp2)
 
-dlmwrite([pathname filename(1:end-4) '_1DH_sum' filename(end-3:end)],[x.' I1_sp2.'],'precision',10,'delimiter',',')
-dlmwrite([pathname filename(1:end-4) '_1DV_sum' filename(end-3:end)],[y.' I2_sp2.'],'precision',10,'delimiter',',')
-dlmwrite([pathname filename(1:end-4) '_1DH_cut' filename(end-3:end)],[x.' crossx_sp2.'],'precision',10,'delimiter',',')
-dlmwrite([pathname filename(1:end-4) '_1DV_cut' filename(end-3:end)],[y.' crossz_sp2.'],'precision',10,'delimiter',',')
+dlmwrite([pathname filename(1:end-4) '_1DH_sum' filename(end-3:end)],[x.' I1.'],'precision',10,'delimiter',',')
+dlmwrite([pathname filename(1:end-4) '_1DV_sum' filename(end-3:end)],[y.' I2.'],'precision',10,'delimiter',',')
+dlmwrite([pathname filename(1:end-4) '_1DH_cut' filename(end-3:end)],[x.' crossx.'],'precision',10,'delimiter',',')
+dlmwrite([pathname filename(1:end-4) '_1DV_cut' filename(end-3:end)],[y.' crossz.'],'precision',10,'delimiter',',')
 % dlmwrite([pathname filename(1:end-4) '_2D' filename(end-3:end)],Anew_sp2,'precision',10,'delimiter',',')
     
     
